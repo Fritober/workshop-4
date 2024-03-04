05ba79d0bc0d8d878e7b48af20aefe8c1d22d92a
Phrito <52508729+Fritober@users.noreply.github.com>
2024-03-04 14:48:06 +0100

    3.2 Create a pair of private and public key for each node

--- src/onionRouters/simpleOnionRouter.ts
+++ src/onionRouters/simpleOnionRouter.ts
@@ -1,7 +1,7 @@
 import bodyParser from "body-parser";
 import express from "express";
 import { BASE_ONION_ROUTER_PORT } from "../config";
-import crypto from "crypto";
+import {generateRsaKeyPair, exportPubKey, importPrvKey, rsaDecrypt, exportPrvKey} from "../crypto";
 
 export async function simpleOnionRouter(nodeId: number) {
   const onionRouter = express();
@@ -27,6 +27,19 @@ export async function simpleOnionRouter(nodeId: number) {
   onionRouter.get("/getLastMessageDestination", (req, res) => {
     res.json({ result: lastMessageDestination });
   });
+
+  onionRouter.get("/getPrivateKey", async (req, res) => {
+        try {
+            const { publicKey, privateKey } = await generateRsaKeyPair(nodeId);
+
+            const privateKeyBase64 = await exportPrvKey(privateKey);
+
+            res.json({ result: privateKeyBase64 });
+        } catch (error) {
+            console.error("Error generating or exporting private key:", error);
+            res.status(500).json({ error: "Internal Server Error" });
+        }
+    });
 
   const server = onionRouter.listen(BASE_ONION_ROUTER_PORT + nodeId, () => {
     console.log(
